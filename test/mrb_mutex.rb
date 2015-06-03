94315f2962f970a2ec84d9fad294243fa8011639
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-06-03 15:39:45 +0900

    Add test

--- test/mrb_mutex.rb
+++ test/mrb_mutex.rb
@@ -2,14 +2,26 @@
 ## Mutex Test
 ##
 
-#assert("Mutex#lock") do
-#  t = Mutex.new
-#  assert_equal(0, t.lock)
-#  t.unlock
-#end
-#
-#assert("Mutex#unclock") do
-#  t = Mutex.new
-#  t.lock
-#  assert_equal(0, t.unlock)
-#end
+assert("Mutex#lock") do
+  t = Mutex.new
+  assert_equal(0, t.lock)
+  t.unlock
+end
+
+assert("Mutex#unclock") do
+  t = Mutex.new
+  t.lock
+  assert_equal(0, t.unlock)
+end
+
+assert("Mutex(:global => true)#lock") do
+  t = Mutex.new :global => true
+  assert_equal(0, t.lock)
+  t.unlock
+end
+
+assert("Mutex(:global => true)#unclock") do
+  t = Mutex.new :global => true
+  t.lock
+  assert_equal(0, t.unlock)
+end
